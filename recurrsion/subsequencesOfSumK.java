c2a8ff898dde975c38d77d4ddef605bfd3229209
TechieTwinkle <bansalgauri08@gmail.com>
2026-04-14 02:09:13 +0530

    Subsequence of sum K

--- recurrsion/subsequencesOfSumK.java
+++ recurrsion/subsequencesOfSumK.java
@@ -1,25 +1,34 @@
 package forGit.recurrsion;
 import java.util.*;
 public class subsequencesOfSumK {
-    public static void subOfSum(int inx,int s, int []arr, int sum, List<Integer> li){
+    public static int subOfSum(int inx,int s, int []arr, int sum, List<Integer> li){
         if(inx == arr.length){
             if(s==sum){
                 System.out.println(li);
+                return 1;
             }
-            return;
+            return 0;
         }
+
+        //picking elements
         li.add(arr[inx]);
         s+=arr[inx];
-        subOfSum(inx+1,s,arr,sum,li);
+        int left = subOfSum(inx+1,s,arr,sum,li);
+
+        //backtracking vapis
         li.remove(li.size() - 1);
         s-=arr[inx];
-        subOfSum(inx+1,s,arr,sum,li);
+
+        // not picked
+        int right = subOfSum(inx+1,s,arr,sum,li);
+
+        return left+right;
     }
     public static void main(String[] args) {
         int [] arr = {1,2,1};
         int sum = 2;
         List<Integer> li = new ArrayList<>();
-        subOfSum(0,0,arr,sum,li);
-        System.out.println(li);
+        int cnt = subOfSum(0,0,arr,sum,li);
+        System.out.println(cnt);
     }
 }
