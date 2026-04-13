be0c02a96123228d7d0c45118fb90a27458b6073
TechieTwinkle <bansalgauri08@gmail.com>
2026-04-13 17:04:50 +0530

    Subsequences

---
+++ recurrsion/subsequences.java
@@ -0,0 +1,21 @@
+package forGit.recurrsion;
+import java.util.*;
+public class subsequences {
+    public static void subsequence(int inx, int [] arr, List<Integer> li){
+        if(inx >= arr.length){
+            System.out.println(li);
+            return;
+        }
+
+        li.add(arr[inx]);
+        subsequence(inx+1,arr,li);
+        li.remove(li.size() - 1);
+        subsequence(inx+1,arr,li);
+    }
+    static void main(String[] args) {
+        int [] arr ={3,1,2};
+        List<Integer> li = new ArrayList<>();
+        subsequence(0,arr,li);
+        System.out.println(li);
+    }
+}
