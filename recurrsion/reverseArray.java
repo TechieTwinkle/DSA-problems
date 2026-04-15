e7419b83f44d44db6d9bd6b570f5c13e1f03a51e
TechieTwinkle <bansalgauri08@gmail.com>
2026-04-15 23:18:13 +0530

    updatedReverseArray

--- recurrsion/reverseArray.java
+++ recurrsion/reverseArray.java
@@ -3,22 +3,23 @@ package forGit.recurrsion;
 public class reverseArray {
     public static void swap(int arr[] , int i, int n){
         int temp = arr[i];
-        arr[i] = arr[n-i-1];
-        arr[n-i-1] = temp;
+        arr[i] = arr[n];
+        arr[n] = temp;
 
     }
         public static void reverse(int i, int arr[], int n){
-        if(i>=n/2) return;
+        if(i>=n) return;
         swap(arr, i, n);
-        reverse(i+1,arr,n);
+        reverse(i+1,arr,n-1);
     }
     static void main(String[] args) {
-        int [] arr = {1,2,3,4};
+        int [] arr = {9,2,10,4};
         int n = arr.length;
-        reverse(0,arr,n);
+        reverse(0,arr,n-1
+        );
 //        swap(arr,0,n-1);
         for(int ele : arr){
-            System.out.println(ele + " ");
+            System.out.print(ele + " ");
         }
 //        System.out.println();
     }
