14baa1dedc42814f4628c9dfc693071fd2213103
Gods Will Khojee <ardiumwing@gmail.com>
2026-07-12 17:45:59 +0530

    Snake pattern

--- MultiDimensionalArrays/OutputIn2DArray.java
+++ MultiDimensionalArrays/OutputIn2DArray.java
@@ -2,22 +2,35 @@ package forGit.MultiDimensionalArrays;
 import java.util.*;
 public class OutputIn2DArray {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-//        int[][] arr = new int[3][4];
-        int[][] arr = {{1,2,3},{4,5,6}};
-        int sum = 0;
-        for(int i =0; i< arr.length; i++){
-            for(int j = 0;j< arr[0].length; j++){
-//                System.out.print(arr[i][j] + " ");
-                arr[i][j] = sc.nextInt();
-            }
-        }
-            System.out.println(sum);
-
-        for(int i =0; i< arr.length; i++){
-            for(int j = 0;j< arr[0].length; j++){
-                System.out.print(arr[i][j] + " ");
+//        Scanner sc = new Scanner(System.in);
+////        int[][] arr = new int[3][4];
+//        int[][] arr = {{1,2,3},{4,5,6}};
+//        int sum = 0;
+//        for(int i =0; i< arr.length; i++){
+//            for(int j = 0;j< arr[0].length; j++){
+////                System.out.print(arr[i][j] + " ");
 //                arr[i][j] = sc.nextInt();
+//            }
+//        }
+//            System.out.println(sum);
+//
+//        for(int i =0; i< arr.length; i++){
+//            for(int j = 0;j< arr[0].length; j++){
+//                System.out.print(arr[i][j] + " ");
+////                arr[i][j] = sc.nextInt();
+//            }
+//            System.out.println();
+
+        int[][] arr = {{1,2,3},{4,5,6}};
+//        for(int j=0; j<arr[0].length;j++){
+//            for(int i= 0; i< arr.length; i++){
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+        for(int[] a : arr){
+            for(int ele : a){
+                System.out.print(ele + " ");
             }
             System.out.println();
         }
