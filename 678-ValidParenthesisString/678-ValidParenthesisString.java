34f3f8ba9cd66cc56359faa6fc1a803e855abd35
Gauri Bansal <bansalgauri08@gmail.com>
2026-08-04 01:36:25 +0530

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.java
@@ -0,0 +1,51 @@
+// Last updated: 04/08/2026, 01:36:24
+class Solution {
+    public boolean checkValidString(String s) {
+
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if(ch == '(') {
+                open.push(i);
+            }
+
+            else if(ch == '*') {
+                star.push(i);
+            }
+
+            else {
+
+                if(!open.isEmpty()) {
+                    open.pop();
+                }
+
+                else if(!star.isEmpty()) {
+                    star.pop();
+                }
+
+                else {
+                    return false;
+                }
+            }
+        }
+
+        // match remaining '(' with later '*'
+        while(!open.isEmpty() && !star.isEmpty()) {
+
+            if(open.peek() < star.peek()) {
+                open.pop();
+                star.pop();
+            }
+
+            else {
+                return false;
+            }
+        }
+
+        return open.isEmpty();
+    }
+}
