f6d6757ec707daf2a655465bf8d9a081998acc0e
Gauri Bansal <bansalgauri08@gmail.com>
2026-09-08 21:29:27 +0530

    Create 2309-GreatestEnglishLetterInUpperAndLowerCase

---
+++ 2309-GreatestEnglishLetterInUpperAndLowerCase/2309-GreatestEnglishLetterInUpperAndLowerCase.java
@@ -0,0 +1,25 @@
+// Last updated: 08/09/2026, 21:29:27
+class Solution {
+    public String greatestLetter(String s) 
+    {
+        char [] arr = s.toCharArray();
+        int n = arr.length;
+        char greatest = 0;
+        for(int i = 0; i<n;i++)
+        {
+            for(int j = n-1 ;j>=0 ;j--)
+            {
+                if(Character.toLowerCase(arr[i]) == Character.toLowerCase(arr[j]) && arr[i]!=arr[j])
+                {
+                    char upper = Character.toUpperCase(arr[i]);
+                    if(upper>greatest)
+                    {
+                        greatest = upper;
+                    }
+                }
+            }
+        }
+        if(greatest == 0) return "";
+        return String.valueOf(greatest);
+    }
+}
